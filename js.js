06d0559af66508e6d415fbd8ad9b81fd86215be3
КонстантинЯ <144781662+KonstantinYa1@users.noreply.github.com>
2023-11-01 17:16:16 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,74 @@
+// 383 1
+
+// const elem = document.getElementById('elem');
+// elem.children[0].style.color = 'ed';
+
+// 383 2
+
+// const elem = document.getElementById('elem');
+// elem.children[elem.children.length - 1].style.color = 'ed';
+
+// 383 3
+
+// const elem = document.getElementById('elem');
+// for (let child of elem.children) {
+//   child.style.color = 'ed';
+// }
+
+// 384 1
+// const elem = document.getElementById('elem');
+// const parent = elem.parentNode;
+// parent.style.border = '1px solid red';
+
+// 385 1
+// const elem = document.getElementById('elem');
+// const parent = elem.closest('div');
+
+
+// 385 2
+// const elem = document.getElementById('elem');
+// const parent = elem.closest('.www');
+
+// 386 1
+// const elem = document.getElementById('elem');
+// const next = elem.nextElementSibling;
+// next.textContent += '!';
+
+// 386 2
+// const elem = document.getElementById('elem');
+// const prev = elem.previousElementSibling;
+// prev.textContent += '!';
+
+// 386 3
+// const elem = document.getElementById('elem');
+// const next = elem.nextElementSibling;
+// const nextNext = next.nextElementSibling;
+// nextNext.textContent += '!';
+
+// 386 4
+// const elem = document.getElementById('elem');
+// const prev = elem.previousElementSibling;
+// const next = elem.nextElementSibling;
+// prev.textContent = next.textContent;
+// next.textContent = elem.textContent;
+
+// 387 1
+// const elem = document.getElementById('elem');
+// elem.textContent = 'Hello, world!';
+
+// 388 1
+// const elems = document.getElementsByTagName('li');
+// for (let i = 0; i < elems.length; i++) {
+//   elems[i].style.color = 'ed';
+// }
+
+// 389 1
+// const elems = document.getElementsByClassName('www');
+// for (let i = 0; i < elems.length; i++) {
+//   elems[i].style.color = 'ed';
+// }
+
+// 390 1
+// const parent = document.querySelector('#parent');
+// const elems1 = parent.querySelectorAll('.www');
+// const elems2 = parent.querySelectorAll('.ggg');
